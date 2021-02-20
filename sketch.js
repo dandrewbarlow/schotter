bbda2fbba1b68fa5b99f5576b9f6e904108237ce
Andrew Barlow <dandrewbarlow@gmail.com>
2021-02-20 15:52:06 -0700

    working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,46 @@
+
+let squareSize = 50;
+
 function setup() {
-  // put setup code here
+  canvas = createCanvas(squareSize * 12, squareSize * 12);
+  noFill();
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  // more for clarity of code than a functional definition
+
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+
+      let offset;
+      if (x == 0) {
+        offset = squareSize + map(random(1), 0, 1, -x * 5/3, x * 5/3);
+      }
+      else {
+        offset = squareSize + map(random(x), 0, x, -x * 5/3, x * 5/3);
+      }
+
+      push();
+
+      // random rotation, in either direction
+      degrees = random(x / 30);
+      if (random(1) > .5) {
+        degrees = -degrees;
+      }
+
+      // use the x & y value to calculate where to draw on the canvas
+      nX = x * squareSize + offset;
+      nY = y * squareSize + offset;
+      console.log(nX, nY);
+      translate(nX, nY);
+
+      rotate(degrees);
+
+      square(0, 0, squareSize);
+
+      pop();
+    }
+  }
+  noLoop();
 }
